382f84dfe53c6dbfbb8042785ac2f349a24f51db
Aristide Sabi <sabiarist@gmail.com>
2021-11-21 04:53:08 +0000

    Random words module Separation Saving favorite words module Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp( const MyApp());
 
@@ -19,56 +19,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class RandomWords  extends StatefulWidget{
 
-  const RandomWords({Key? key}) : super(key: key);
 
-  @override
-  State<StatefulWidget> createState() => _RandomWords();
-}
 
-class _RandomWords extends State<RandomWords>{
-
-  final _randomWordPairs = <WordPair>[];
-
-  @override
-  Widget build(BuildContext context) {
-
-    Widget _buildList() {
-      return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, item) {
-          if (item.isOdd) return Divider();
-          final index = item ~/ 2;
-          if (index >= _randomWordPairs.length){
-            _randomWordPairs.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_randomWordPairs[index]);
-        },
-      );
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Wordpair Generator'),
-      ),
-      body: _buildList(),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        color: Colors.deepPurple,
-        child: Container(height: 50.0),
-      ),
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text( pair.asPascalCase,
-      style: const TextStyle(
-        fontSize: 18.0,
-      ),
-      ),
-    );
-  }
-}
 
